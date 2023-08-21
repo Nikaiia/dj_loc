6c89754c899973e665dfd6604083894bc6772b44
Maria Coelho <mariiacoelho25@hotmail.com>
2023-08-21 13:56:42 +0200

    reviews and users controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,36 @@
+class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_review, only: :show
+  
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def create
+    @review = current_user.reviews.build(review_params)
+    if @review
+      redirect_to review_path(@review)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :booking_id)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+end
