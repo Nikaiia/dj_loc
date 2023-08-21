dc98ea9797eee4d82f7fe1bcbdb6d084a2e727b1
Brice <108925232+Nikaiia@users.noreply.github.com>
2023-08-21 12:00:34 +0200

    model generation user / booking

---
+++ db/migrate/20230821095650_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.string :phone_number
+      t.string :role
+
+      t.timestamps
+    end
+  end
+end
