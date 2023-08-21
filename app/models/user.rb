3472c2049836ef66788c2785fbf61a4047d66d05
Brice <108925232+Nikaiia@users.noreply.github.com>
2023-08-21 16:41:17 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :offers, dependent: :destroy
-  has_many :bookings, dependent: :destroy
+  has_many :bookings
 
   validates :first_name, presence: true
   validates :last_name, presence: true
