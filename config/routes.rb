45ecf2a9ce2d90c127891ce4e358e07dde3270d5
Lyes Mehaddene <lyes.mehaddene@edhec.com>
2023-08-24 17:40:54 +0200

    fixed bookings and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :offers do
-    resources :bookings, only: [:new, :create, :show]
-  end
+    resources :bookings, only: [:new, :create, :show, :index] do
+      member do
+        post :accept_booking
+        post :decline_booking
+      end
+    end
+      end
+
 
   resources :users, except: [:index] do
     resources :bookings, only: [:index, :destroy, :show]
