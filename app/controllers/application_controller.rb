9833e8a1b7a3e0e8f1f49ff5df6c639e7bcce1bd
Brice <108925232+Nikaiia@users.noreply.github.com>
2023-08-25 12:58:44 +0200

    Add view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name username role photo])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name username role photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name username role photo email])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role, :photo)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role, :photo, :username)
   end
 
   def set_user
